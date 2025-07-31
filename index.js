c435be1ed9defe3de2f58af1add396bb52e2a6df
grecoww <132113259+grecoww@users.noreply.github.com>
2025-07-31 15:56:32 -0300

    obtain required info from defaulters athletes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import token from './token.json' with { type: 'json' };
 import { google } from 'googleapis';
+import Fuse from 'fuse.js';
 
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 
@@ -12,8 +13,61 @@ const sheets = google.sheets({
   auth: client,
 });
 
-const res = await sheets.spreadsheets.values.get({
-  spreadsheetId: process.env.PLANILHA_INFO_ATLETAS,
-  range: 'Página1!B2:B',
-});
-console.log(res.data.values);
+async function GetDefaultersNames() {
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.PLANILHA_PAGAMENTO_ATLETAS,
+    range: 'Mensalidades!B5:E',
+  });
+  const paymentsInfo = res.data.values;
+
+  const parsedPaymentsInfo = paymentsInfo
+    .filter((row) => {
+      return (
+        !!row[0] &&
+        row[0] !== '#N/A' &&
+        row[0] !== 'Total' &&
+        row[3] === 'Não Pago'
+      );
+    })
+    .map((row) => {
+      return row[0];
+    });
+  return parsedPaymentsInfo;
+}
+
+async function GetUsersInfo(nameArray) {
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.PLANILHA_INFO_ATLETAS,
+    range: 'Página1!B2:F',
+  });
+  const usersInfo = res.data.values;
+
+  const options = {
+    keys: [[0]],
+    ignoreDiacritics: true,
+    includeScore: true,
+    minMatchCharLength: 3,
+  };
+  const fuse = new Fuse(usersInfo, options);
+
+  let targetInfoList = [];
+  for (const name of nameArray) {
+    const result = fuse.search(name);
+    const parsedResult = result[0].item;
+
+    const targetInfo = {
+      name: parsedResult[0],
+      nickname: parsedResult[1] || null,
+      phoneNumber: parsedResult[4],
+    };
+    targetInfoList.push(targetInfo);
+  }
+
+  return targetInfoList;
+}
+
+const defaultersNames = await GetDefaultersNames();
+
+const defaultersInfos = await GetUsersInfo(defaultersNames);
+
+console.log(defaultersInfos);
